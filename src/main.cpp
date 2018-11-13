2714c80ee3f0123c53996dfac9a48de3bea2e98e
Oleksandr Gavuka <oleksandr.gavuka@masterofcode.com>
2018-11-13 19:55:32 +0200

    work without out from while

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,16 @@
 
 Encoder myEnc(3, 7);
 long oldPosition = 0;
-const byte buttonPin = 2;
-int buttonState = 0;
-int ctrlFan = 0;
-const int fanPin = 9;
+int buttonEncState = 0;
 
-int get_temperature_period = 250;
-unsigned long time_now = 0;
+const byte buttonEncPin = 2;
+int tempBtnOnPin1 = A4;
+int tempBtnOnPin2 = A5;
+int powerPin1 = A1;
+int powerPin2 = A3;
+int thermopare = A0;
+int systemFan = 13;
+const int fanPin = 9;
 
 int temperature1 = 0;
 int temperature2 = 0;
@@ -20,32 +23,37 @@ int ctrlTemp1 = 0;
 int ctrlTemp2 = 0;
 int tempBtnOnState1 = false;
 int tempBtnOnState2 = false;
-int tempBtnOnPin1 = A0;
-int tempBtnOnPin2 = A1;
-int powerPin1 = A2;
-int powerPin2 = A3;
+int ctrlFan = 0;
+
+int get_temperature_period = 250;
+unsigned long time_now = 0;
 
 void button()
 {
-    if (buttonState < 3)
-        buttonState++;
-    else if (buttonState > 3)
-        buttonState = 0;
+    if (buttonEncState < 3)
+        buttonEncState++;
+    else if (buttonEncState > 3)
+        buttonEncState = 0;
     else
-        buttonState++;
+        buttonEncState++;
 }
 
 void setup()
 {
     max7219Init();
     max6675Init();
-    pinMode(buttonPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(buttonPin), button, FALLING);
+    attachInterrupt(digitalPinToInterrupt(buttonEncPin), button, FALLING);
     Timer1.initialize(34); // 40 us = 25 kHz
-    pinMode(tempBtnOnPin1, INPUT_PULLUP);
-    pinMode(tempBtnOnPin1, INPUT_PULLUP);
-    pinMode(powerPin1,  OUTPUT);
-    pinMode(powerPin2,  OUTPUT);
+    // Timer1.initialize(20); // 40 us = 25 kHz
+
+    pinMode(tempBtnOnPin1, INPUT);
+    pinMode(tempBtnOnPin2, INPUT);
+    pinMode(powerPin1, OUTPUT);
+    pinMode(powerPin2, OUTPUT);
+    pinMode(buttonEncPin, INPUT_PULLUP);
+    pinMode(thermopare, INPUT);
+    pinMode(systemFan, OUTPUT);
+    // pinMode(fanPin, OUTPUT);
 }
 
 int encCtrl(int limit)
@@ -87,71 +95,126 @@ void loop()
     print(ctrlFan, 3);
 
     /**
-     * *****TEMPERATURE CONTROL*****
-    */
-    //if soldering iron ON
-    if(tempBtnOnState1){
-        if(temperature1 < ctrlTemp1) digitalWrite(powerPin1, HIGH); //on
-        if(temperature1 > ctrlTemp1) digitalWrite(powerPin1, LOW); //of
-    }
-    //if soldering iron OFF
-    if(!tempBtnOnState1){
-        digitalWrite(powerPin1, LOW); //of
-    }
-    //if soldering Hairdryer ON
-    else if(tempBtnOnState2){
-        if(temperature2 < ctrlTemp2) digitalWrite(powerPin2, HIGH); //on
-        if(temperature2 > ctrlTemp2) digitalWrite(powerPin2, LOW); //of
-        if(temperature2 > 510){
-            digitalWrite(powerPin2, LOW); //off
-            ctrlFan = 100;
-        }
-    }
-    //if soldering Hairdryer OFF
-    else if (!tempBtnOnState2)
-    {
-        if(temperature2 > 400) ctrlFan = 100;
-        if(temperature2 > 300) ctrlFan = 80;
-        if(temperature2 > 150) ctrlFan = 50;
-        if(temperature2 > 100) ctrlFan = 30;
-        if(temperature2 > 70) ctrlFan = 20;
-        if(temperature2 < 50) ctrlFan = 0;
-    }
-    
-    /**
      * *****MENU*****
     */
-    // if (buttonState == 0)
-    // {
-    // }
-    /* else  */if (buttonState == 1)
+    if (buttonEncState == 0)
     {
+        while (buttonEncState == 0)
+        {
+            if(buttonEncState != 0) break;
+            /**
+     * GET BUTTONS STATE
+    */
+            tempBtnOnState1 = digitalRead(tempBtnOnPin1);
+            tempBtnOnState2 = digitalRead(tempBtnOnPin2);
+            /**
+     * GET TEMPERATURE BY INTERVAL
+    */
+            if (millis() > time_now + get_temperature_period)
+            {
+                time_now = millis();
+                temperature1 = (int)(getByCelsius1());
+                temperature2 = (int)(getByCelsius2());
+            }
+
+            /**
+     * MAIN DISPLAY
+    */
+            print(temperature1, 1);
+            print(temperature2, 2);
+            print(ctrlFan, 3);
+
+            /**
+     * *****TEMPERATURE CONTROL*****
+    */
+            //if soldering iron ON
+            if (tempBtnOnState1)
+            {
+                if (temperature1 < ctrlTemp1)
+                    digitalWrite(powerPin1, HIGH); //on
+                if (temperature1 >= ctrlTemp1)
+                    digitalWrite(powerPin1, LOW); //of
+            }
+            //if soldering iron OFF
+            if (!tempBtnOnState1)
+            {
+                digitalWrite(powerPin1, LOW); //of
+            }
+            //if soldering Hairdryer ON
+            if (tempBtnOnState2)
+            {
+                // enable fan
+                if (ctrlFan < 10)
+                {
+                    ctrlFan = 10;
+                    // Timer1.pwm(fanPin, ((float)ctrlFan / 100) * 1023);
+                    delay(3000);
+                }
+                // temperature control
+                if (temperature2 < ctrlTemp2)
+                    digitalWrite(powerPin2, HIGH); //on
+                if (temperature2 >= ctrlTemp2)
+                    digitalWrite(powerPin2, LOW); //of
+                if (temperature2 > 510)
+                {
+                    digitalWrite(powerPin2, LOW); //off
+                    ctrlFan = 100;
+                }
+            }
+            //if soldering Hairdryer OFF
+            if (!tempBtnOnState2)
+            {
+                digitalWrite(powerPin2, LOW); //off
+
+                if (temperature2 > 400)
+                    ctrlFan = 100;
+                if (temperature2 > 300)
+                    ctrlFan = 80;
+                if (temperature2 > 150)
+                    ctrlFan = 50;
+                if (temperature2 > 100)
+                    ctrlFan = 30;
+                if (temperature2 > 70)
+                    ctrlFan = 20;
+                if (temperature2 < 50)
+                    ctrlFan = 0;
+            }
+        }
+    }
+    if (buttonEncState == 1)
+    {
+        digitalWrite(powerPin2, LOW); //of
+        digitalWrite(powerPin1, LOW); //of
         oldPosition = ctrlTemp1;
         myEnc.write(ctrlTemp1);
 
-        while (buttonState == 1)
+        while (buttonEncState == 1)
         {
             ctrlTemp1 = encCtrl(500);
             print(ctrlTemp1, 1);
         }
     }
-    else if (buttonState == 2)
+    if (buttonEncState == 2)
     {
+        digitalWrite(powerPin2, LOW); //of
+        digitalWrite(powerPin1, LOW); //of
         oldPosition = ctrlTemp2;
         myEnc.write(ctrlTemp2);
 
-        while (buttonState == 2)
+        while (buttonEncState == 2)
         {
             ctrlTemp2 = encCtrl(500);
             print(ctrlTemp2, 2);
         }
     }
-    else if (buttonState == 3)
+    if (buttonEncState == 3)
     {
+        digitalWrite(powerPin2, LOW); //of
+        digitalWrite(powerPin1, LOW); //of
         oldPosition = ctrlFan;
         myEnc.write(ctrlFan);
 
-        while (buttonState == 3)
+        while (buttonEncState == 3)
         {
             ctrlFan = encCtrl(100);
             print(ctrlFan, 3);
